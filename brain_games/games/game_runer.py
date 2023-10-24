0e9bf98349b7fe6159fe801a5a9cb9865d5db946
wiktorbgu <wiktorbgu@gmail.com>
2023-10-24 20:23:35 +0300

    fix update

--- brain_games/games/game_runer.py
+++ brain_games/games/game_runer.py
@@ -6,18 +6,15 @@ USERNAME = welcome_user()
 
 def run_game(game):
     current_round = 0
-    success_game = True
-    while (success_game and current_round < ROUND_GAME):
+    while (current_round < ROUND_GAME):
         current_round += 1
         user_answer, correct_result = game(current_round)
         if user_answer == correct_result:
             print('Correct!')
         else:
-            success_game = False
             print(f"'{user_answer}' is wrong answer ;(. ", end='')
             print(f"Correct answer was '{correct_result}'.")
+            print(f"Let's try again, {USERNAME}!")
             break
-    if success_game:
+    if current_round == ROUND_GAME:
         print(f'Congratulations, {USERNAME}!')
-    else:
-        print(f"Let's try again, {USERNAME}!")
